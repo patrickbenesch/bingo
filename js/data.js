e0c5c23d805d0d0a4e0aa93b95cc4b4028094192
patrickbenesch <patrick.benesch@controlware.de>
2017-04-13 17:26:48 +0200

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,20 +1,20 @@
 
-var headerText = "Bingo";
+var headerText = "CW CLOUD BINGO";
 
 var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
 
-var winText = "Winner";
+var winText = "Gewinner!";
 
 var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
+        {"square": "On Premise"},
+        {"square": "Cloud"},
+        {"square": "In der Cloud wird geklaut"},
+        {"square": "Next Generation"},
+        {"square": "Azure"},
         {"square": "six"},
         {"square": "seven"},
         {"square": "eight"},
@@ -30,7 +30,7 @@ var JSONBingo = {"squares": [
         {"square": "eighteen"},
         {"square": "nineteen"},
         {"square": "twenty"},
-        {"square": "twenty one"},
+        {"square": "AWS"},
         {"square": "twenty two"},
         {"square": "twenty three"},
         {"square": "twenty four"},    	
